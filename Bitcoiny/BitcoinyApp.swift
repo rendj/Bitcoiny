f6d96d5c89efce2096975ccc2e775fd45a942f1d
Andrii Popov <popov.andrii@gmail.com>
2025-09-23 12:05:17 +0200

    add initial project

---
+++ Bitcoiny/BitcoinyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinyApp.swift
+//  Bitcoiny
+//
+//  Created by Andrii Popov on 23.09.25.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
